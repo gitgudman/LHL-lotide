dde8f6649c835cab2d4ade97511ab1a164f65c6d
gitgudman <michaelquach604@gmail.com>
2025-05-20 09:00:35 -0700

    refract final

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,11 @@
-// eqArrays function copied
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  
-  return true;
-};
-  
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-  
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", 3], ["1", "2", "3"]);
-  
+
+module.exports = assertArraysEqual;
